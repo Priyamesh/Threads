5480efebf46e56be1f5e435770ab7a787f7e3813
Priyamesh <priyamesh28@gmail.com>
2024-09-12 20:24:13 +0530

    Added search page

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Activities</div>;
+};
+
+export default page;
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { FilterQuery, SortOrder } from "mongoose";
+
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
@@ -80,6 +82,57 @@ export async function fetchUserPosts(userId: string) {
     return threads;
   } catch (error) {
     console.error("Error fetching user threads:", error);
+    throw error;
+  }
+}
+
+// Almost similar to Thead (search + pagination) and Community (search + pagination)
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+
+    // Updated query definition with index signature
+    const query: FilterQuery<typeof User> & { [key: string]: any } = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      (query as any).$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
     throw error;
   }
 }
